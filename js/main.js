5b3643c4e8de33889249c03ee56021a4319b8fcf
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-16 17:27:57 -0600

    add some randomosity

--- js/main.js
+++ js/main.js
@@ -1,61 +1,16 @@
 $(document).ready(function(){
-  $('.top-left').hover(
+
+  $('.screen-divider > *').hover(
     function(){
-      // change the icon position
-      $('.icon1').addClass('icon1open');
-      $('.icon1').css('padding','15px');
-      $('.icon1').css('border-radius','20%');
+      $(this).addClass('bgChange');
+      $('.icon1').addClass('iconChange');
     },
     function(){
-      // change the icon position back
-      $('.icon1').removeClass('icon1open');
-      $('.icon1').css('padding','0px');
-      $('.icon1').css('border-radius','0');
+      $(this).removeClass('bgChange');
+      $('.icon1').removeClass('iconChange');
     }
   );
 
-  $('.bottom-left').hover(
-    function(){
-      // change the icon position
-      $('.icon2').addClass('icon2open');
-      $('.icon2').css('padding','15px');
-      $('.icon2').css('border-radius','20%');
-    },
-    function(){
-      // change the icon position back
-      $('.icon2').removeClass('icon2open');
-      $('.icon2').css('padding','0px');
-      $('.icon2').css('border-radius','0');
-    }
-  );
 
-  $('.top-right').hover(
-    function(){
-      // change the icon position
-      $('.icon3').addClass('icon3open');
-      $('.icon3').css('padding','15px');
-      $('.icon3').css('border-radius','20%');
-    },
-    function(){
-      // change the icon position back
-      $('.icon3').removeClass('icon3open');
-      $('.icon3').css('padding','0px');
-      $('.icon3').css('border-radius','0');
-    }
-  );
 
-  $('.bottom-right').hover(
-    function(){
-      // change the icon position
-      $('.icon4').addClass('icon4open');
-      $('.icon4').css('padding','15px');
-      $('.icon4').css('border-radius','20%');
-    },
-    function(){
-      // change the icon position back
-      $('.icon4').removeClass('icon4open');
-      $('.icon4').css('padding','0px');
-      $('.icon4').css('border-radius','0');
-    }
-  );
 });
