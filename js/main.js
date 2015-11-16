fb425b32e778e6ada23afb08844a9fb86db6640a
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-16 17:38:22 -0600

    deleted stupid shit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
 
-  $('.screen-divider > *').hover(
-    function(){
-      $(this).addClass('bgChange');
-      $('.icon1').addClass('iconChange');
-    },
-    function(){
-      $(this).removeClass('bgChange');
-      $('.icon1').removeClass('iconChange');
-    }
-  );
 
 
 
