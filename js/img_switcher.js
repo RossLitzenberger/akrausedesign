80c7dc2e1edac8837328ec16510faf5b7a93d983
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-17 02:32:18 -0600

    add shit loads of good stuff

---
+++ js/img_switcher.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.column-layout').click(function(){
+    $(this).find('img').toggle();
+  });
+  $('.row-layout').click(function(){
+    $(this).find('img').toggle();
+  });
+  $('.border-layout').click(function(){
+    $(this).find('img').toggle();
+  });
+});
+
+/*
+$("#infoToggler").click(function() {
+    $(this).find('img').toggle();
+});​
+*/
--- js/main.js
+++ js/main.js
@@ -8,27 +8,27 @@ $(document).ready(function(){
   });
 
   $('.column-layout').click(function(){
-    $('.top-left').toggleClass('col-top-left')
-    $('.bottom-left').toggleClass('col-bottom-left');
-    $('.top-right').toggleClass('col-top-right');
-    $('.bottom-right').toggleClass('col-bottom-right');
-    $('.changer').slideToggle('fast');
+    $('#top-left').toggleClass('def-top-left col-top-left');
+    $('#bottom-left').toggleClass('def-bottom-left col-bottom-left');
+    $('#top-right').toggleClass('def-top-right col-top-right');
+    $('#bottom-right').toggleClass('def-bottom-right col-bottom-right');
+
+    // grey out the other icons, except for the default one,
+    // user must go back to the default view first.
+    $('.row-layout').toggle('fast');
+    $('.border-layout').toggle('fast');
   });
 
   $('.row-layout').click(function(){
-    $('.top-left').toggleClass('row-top-left');
-    $('.bottom-left').toggleClass('row-bottom-left');
-    $('.top-right').toggleClass('row-top-right');
-    $('.bottom-right').toggleClass('row-bottom-right');
-    $('.changer').slideToggle('fast');
+    $('#top-left').toggleClass('def-top-left row-top-left');
+    $('#bottom-left').toggleClass('def-bottom-left row-bottom-left');
+    $('#top-right').toggleClass('def-top-right row-top-right');
+    $('#bottom-right').toggleClass('def-bottom-right row-bottom-right');
+
+    // grey out the other icons, except for the default one,
+    // user must go back to the default view first.
+    $('.column-layout').toggle('fast');
+    $('.border-layout').toggle('fast');
   });
 
-
-/*
-  $('.border-layout').click(function(){
-    $('.top-left');
-    $('.bottom-left');
-    $('.top-right');
-    $('.bottom-right');
-  }); */
 });
