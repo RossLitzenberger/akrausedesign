a9ac784dc32f32a31aa365aa7be1822a0a4fb7a4
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-16 23:06:31 -0600

    add viewport change css *beginning shit*

--- js/main.js
+++ js/main.js
@@ -4,6 +4,31 @@ $(document).ready(function(){
 
 
   $('.pull-me').click(function(){
-    $('.changer').slideToggle();
+    $('.changer').slideToggle('fast');
   });
+
+  $('.column-layout').click(function(){
+    $('.top-left').toggleClass('col-top-left')
+    $('.bottom-left').toggleClass('col-bottom-left');
+    $('.top-right').toggleClass('col-top-right');
+    $('.bottom-right').toggleClass('col-bottom-right');
+    $('.changer').slideToggle('fast');
+  });
+
+  $('.row-layout').click(function(){
+    $('.top-left').toggleClass('row-top-left');
+    $('.bottom-left').toggleClass('row-bottom-left');
+    $('.top-right').toggleClass('row-top-right');
+    $('.bottom-right').toggleClass('row-bottom-right');
+    $('.changer').slideToggle('fast');
+  });
+
+
+/*
+  $('.border-layout').click(function(){
+    $('.top-left');
+    $('.bottom-left');
+    $('.top-right');
+    $('.bottom-right');
+  }); */
 });
