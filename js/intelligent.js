9cce2b45df297a1a72e472fecb0d82bb2c81cf79
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-18 14:16:12 -0600

    delete stupid ideas

--- js/intelligent.js
+++ js/intelligent.js
@@ -1,100 +1,25 @@
 // intelligent.js
 
-var a = 5;
-var b = 5;
-var c = 5;
-var d = 5;
+var a = ['.def-top-left','.def-bottom-left','def-top-right','def-bottom-right'];
+var b = ['.col-top-right','col-bottom-left','col-top-right','col-bottom-right'];
+var c = ['.row-top-right','row-bottom-left','row-top-right','row-bottom-right'];
+var d = ['border-top-right','border-bottom-left','border-top-right','border-bottom-right'];
 
 // count how many options for the array
 var z = 1;
 
 
-for(var i = 1; i < a; i++)
+for(var i = 1; i < a[].length; i++)
 {
-  for(var j = 1; j < b; j++)
+  for(var j = 1; j < b.length; j++)
   {
-    for(var f = 1; f < c; f++)
+    for(var f = 1; f < c.length; f++)
     {
-      for(var e = 1; e < d; e++)
+      for(var e = 1; e < d.length; e++)
       {
-        ++z;
+
       }
     }
   }
 }
 console.log('lines: ' + z);
-
-// default, column, row, border
-
-var default_layout = {
-  1: [$(".def-top-left")],
-  2: [$(".def-bottom-left")],
-  3: [$(".def-top-right")],
-  4: [$(".def-bottom-right")]
-};
-
-
-
-var column_layout = {
-  1: [$(".col-top-left")],
-  2: [$(".col-bottom-left")],
-  3: [$(".col-top-right")],
-  4: [$(".col-bottom-right")]
-};
-
-
-
-var row_layout = {
-  1: [$(".row-top-left")],
-  2: [$(".row-bottom-left")],
-  3: [$(".row-top-right")],
-  4: [$(".row-bottom-right")]
-};
-
-
-
-var border_layout = {
-  1: [$(".border-top-left")],
-  2: [$(".border-bottom-right")],
-  3: [$(".border-top-right")],
-  4: [$(".border-bottom-right")]
-};
-
-
-
-
-
-
-/*
-  create a click function that lets the system
-  figure out what classes to add or remove on its own
-*/
-
-
-
-console.log(border_layout.topLeft);
-console.log(border_layout.bottomLeft);
-console.log(border_layout.topRight);
-console.log(border_layout.bottomRight);
-
-
-// print out the length of the objects
-// (should be 4 for each)
-var defaultObjectLength = Object.keys(default_layout).length;
-var columnObjectLength = Object.keys(column_layout).length;
-var rowObjectLength = Object.keys(row_layout).length;
-var borderObjectLength = Object.keys(border_layout).length;
-
-console.log(defaultObjectLength);
-console.log(columnObjectLength);
-console.log(rowObjectLength);
-console.log(borderObjectLength);
-
-var arr = [];
-for(var i in default_layout)
-{
-  if(default_layout.hasOwnProperty(i)){
-    arr.push(default_layout[i]);
-  }
-}
-console.log(arr);
