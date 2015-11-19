b598971011f59394976731f1f1620a1cfc7fb2cc
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-18 22:11:12 -0600

    js optimization

--- js/ui-changer.js
+++ js/ui-changer.js
@@ -31,12 +31,6 @@ $(document).ready(function(){
     $('#bottom-left').toggleClass('def-bottom-left col-bottom-left');
     $('#top-right').toggleClass('def-top-right col-top-right');
     $('#bottom-right').toggleClass('def-bottom-right col-bottom-right');
-
-    // toggle the icons to new positions
-    $('#tl-icon').toggleClass('icon1 icon1-column');
-    $('#bl-icon').toggleClass('icon2 icon2-column');
-    $('#tr-icon').toggleClass('icon3 icon3-column');
-    $('#br-icon').toggleClass('icon4 icon4-column');
   });
 
   $('#row-layout').click(function(){
@@ -50,12 +44,6 @@ $(document).ready(function(){
     $('#bottom-left').toggleClass('def-bottom-left row-bottom-left');
     $('#top-right').toggleClass('def-top-right row-top-right');
     $('#bottom-right').toggleClass('def-bottom-right row-bottom-right');
-
-    // toggle the icons to new positions
-    $('#tl-icon').toggleClass('icon1 icon1-row');
-    $('#bl-icon').toggleClass('icon2 icon2-row');
-    $('#tr-icon').toggleClass('icon3 icon3-row');
-    $('#br-icon').toggleClass('icon4 icon4-row');
   });
 
   $('#border-layout').click(function(){
@@ -69,18 +57,32 @@ $(document).ready(function(){
     $('#bottom-left').toggleClass('def-bottom-left border-bottom-left');
     $('#top-right').toggleClass('def-top-right border-top-right');
     $('#bottom-right').toggleClass('def-bottom-right border-bottom-right');
-
-    // toggle the icons to new positions
-    $('#tl-icon').toggleClass('icon1 icon1-border');
-    $('#bl-icon').toggleClass('icon2 icon2-border');
-    $('#tr-icon').toggleClass('icon3 icon3-border');
-    $('#br-icon').toggleClass('icon4 icon4-border');
   });
 
   $('#default-layout').click(function(){
     $('#row-layout').toggle('fast');
     $('#column-layout').toggle('fast');
     $('#border-layout').toggle('fast');
+  });
+
+  $('.def-top-left').click(function(){
+    $(this).toggleClass('def-top-left-open');
+    $(this).toggleClass('importance');
+  });
+
+  $('.def-bottom-left').click(function(){
+    $(this).toggleClass('def-bottom-left-open');
+    $(this).toggleClass('importance');
+  });
+
+  $('.def-top-right').click(function(){
+    $(this).toggleClass('def-top-right-open');
+    $(this).toggleClass('importance');
+  });
+
+  $('.def-bottom-right').click(function(){
+    $(this).toggleClass('def-bottom-right-open');
+    $(this).toggleClass('importance');
   });
 
 });
