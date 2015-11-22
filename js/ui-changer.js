39961dc3d1fbc10b992dbd2d2acf783aedefd565
Andrew Krause <andrew.l.krause@gmail.com>
2015-11-22 01:45:08 -0600

    add comments to the confusing js file

--- js/ui-changer.js
+++ js/ui-changer.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
 
   // remove these classes when another layout has been triggered
 
-  $('.def-top-left').click(function(){
+  $('#top-left').click(function(){
     $(this).toggleClass('def-top-left def-top-left-open');
   });
-  $('.def-bottom-left').click(function(){
+  $('#bottom-left').click(function(){
     $(this).toggleClass('def-bottom-left def-bottom-left-open');
   });
-  $('.def-top-right').click(function(){
+  $('#top-right').click(function(){
     $(this).toggleClass('def-top-right def-top-right-open');
   });
-  $('.def-bottom-right').click(function(){
+  $('#bottom-right').click(function(){
     $(this).toggleClass('def-bottom-right def-bottom-right-open');
   });
 
@@ -45,7 +45,11 @@ $(document).ready(function(){
 
 
   $('#column-layout').click(function(){
+    // toggle the images when they are clicked
     $(this).find('img').toggle();
+
+    // hide the other buttons so that you have to go
+    // back to the default layout first
     $('#row-layout').toggle('fast');
     $('#border-layout').toggle('fast');
     $('#default-layout').toggle('fast');
@@ -58,24 +62,32 @@ $(document).ready(function(){
 
 
     // something here that makes the windows open
-    
+
     // windows not opening correclty
     $('.col-top-left').click(function(){
+      // toggle between open and close
       $(this).toggleClass('col-top-left col-top-left-open');
     });
     $('.col-bottom-left').click(function(){
+      // toggle between open and close
       $(this).toggleClass('col-bottom-left col-bottom-left-open');
     });
     $('.col-top-right').click(function(){
+      // toggle between open and close
       $(this).toggleClass('col-top-right col-top-right-open');
     });
     $('.col-bottom-right').click(function(){
+      // toggle between open and close
       $(this).toggleClass('col-bottom-right col-bottom-right-open');
     });
   });
 
   $('#row-layout').click(function(){
+    // toggle between the different images
     $(this).find('img').toggle();
+
+    // toggle on and off the other images so the user
+    // must go back to the default layout first.
     $('#column-layout').toggle('fast');
     $('#border-layout').toggle('fast');
     $('#default-layout').toggle('fast');
@@ -90,21 +102,29 @@ $(document).ready(function(){
     // something here that makes the windows open
 
     $('#top-left').click(function(){
+      // toggle between open and close
       $(this).toggleClass('row-top-left row-top-left-open');
     });
     $('#bottom-left').click(function(){
+      // toggle between open and close
       $(this).toggleClass('row-bottom-left row-bottom-left-open');
     });
     $('#top-right').click(function(){
+      // toggle between open and close
       $(this).toggleClass('row-top-right row-top-right-open');
     });
     $('#bottom-right').click(function(){
+      // toggle between open and close
       $(this).toggleClass('row-bottom-right row-bottom-right-open');
     });
   });
 
   $('#border-layout').click(function(){
+    // toggle between the two images
     $(this).find('img').toggle();
+
+    // toggle on and off the other images so that
+    // the user has to go back to the default layout first.
     $('#row-layout').toggle('fast');
     $('#column-layout').toggle('fast');
     $('#default-layout').toggle('fast');
@@ -119,20 +139,27 @@ $(document).ready(function(){
     // something here that makes the windows open
 
     $('#top-left').click(function(){
+      // toggle between open and close
       $(this).toggleClass('border-top-left border-top-left-open');
     });
     $('#bottom-left').click(function(){
+      // toggle between open and close
       $(this).toggleClass('border-bottom-left border-bottom-left-open');
     });
     $('#top-right').click(function(){
+      // toggle between open and close
       $(this).toggleClass('border-top-right border-top-right-open');
     });
     $('#bottom-right').click(function(){
+      // toggle between open and close
       $(this).toggleClass('border-bottom-right border-bottom-right-open');
     });
   });
 
   $('#default-layout').click(function(){
+    // these arent required because it is already in default
+    // mode, but I just want to follow the same style as
+    // with the others. 
     $('#row-layout').toggle('fast');
     $('#column-layout').toggle('fast');
     $('#border-layout').toggle('fast');
