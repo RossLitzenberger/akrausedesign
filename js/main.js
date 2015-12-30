05f6dfff9993c5c4d0f5e15d81c3a2d8cf1db970
Andrew Krause <andrew.l.krause@gmail.com>
2015-12-30 15:43:15 -0600

    add click importance changing functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 
 
 
-
 $('.home_btn').click(function(){
   if($('.about_me').is(':visible') && $('.about_me').hasClass('active')){
     $('.about_me').slideToggle('active');
@@ -23,9 +22,21 @@ $('.about_btn').click(function(){
   $('.about_me').slideToggle('fast');
   $('.home_btn').toggleClass('active');
   $(this).toggleClass('active');
-})
+});
 
 
 $('.blog_btn').click(function(){
-  $('#blogs').slideToggle();
+  $('#blogs').slideToggle('fast');
 });
+$('.about_me').click(function(){
+  // hide any pages in front of this if it is clicked
+  if($('#blogs').is(':visible')){
+    $('#blogs').slideToggle('fast');
+    console.log('hidden');
+  }
+  else{
+    $(this).slideToggle();
+    $('.about_me').removeClass('active');
+    $('.home_btn').toggleClass('active');
+  }
+})
