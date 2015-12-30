5e20db1494f0a3484b3863c980ca87eb3d984c69
Andrew Krause <andrew.l.krause@gmail.com>
2015-12-30 14:20:45 -0600

    remove scrolling functions

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-var about = $('#about').offset().top;
-var bottom = $('#about').offset().bottom;
-$(window).scroll(function(){
-  // var wScroll = $(this).scrollTop();
-  // console.log(wScroll);
 
-
-});
 
 
 
 $('#discover').click(function(){
   $('.about_me').slideToggle("fast");
+  $('.about_btn').toggleClass('blur');
 });
 
 
 $('.about_btn').click(function(){
   $('.about_me').slideToggle('fast');
+  $(this).toggleClass('blur');
 })
 
 
 $('.blog_btn').click(function(){
-  $('#blogS').slideToggle("fast");
+  $('#blogs').slideToggle("fast");
 });
+
+if($('#blogs').is(":visible")){
+  $('.main_nav').css('display','block');
+}
+else{
+  $('.main_nav').css('display','inline-block');
+}
