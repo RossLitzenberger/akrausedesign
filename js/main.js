459190c0df6b2a24c600ae03fe1ebac05c6dcf1f
Andrew Krause <andrew.l.krause@gmail.com>
2015-12-30 16:03:49 -0600

    allow all pages to hide when about is clicked"

--- js/main.js
+++ js/main.js
@@ -28,7 +28,18 @@ $('.about_me').click(function(){
   // hide any pages in front of this if it is clicked
   if($('#blogs').is(':visible')){
     $('#blogs').slideToggle('fast');
-    console.log('hidden');
+
+    if($('#contact').is(':visible')){
+      $('#contact').slideToggle('fast');
+    }
+    else{
+      console.log('contact is already hidden');
+    }
+
+    console.log('pages hidden');
+  }
+  else if($('#contact').is(':visible')){
+    $('#contact').slideToggle('fast');
   }
   else{
     $(this).slideToggle();
@@ -43,6 +54,15 @@ $('.blog_btn').click(function(){
 
 $('#blogs').click(function(){
   if($(this).is(":visible")){
-    $(this).slideToggle();
+    $(this).slideToggle('fast');
   }
-})
+});
+$('.contact_btn').click(function(){
+  $('#contact').slideToggle('fast');
+});
+
+$('#contact').click(function(){
+  if($(this).is(':visible')){
+    $(this).slideToggle('fast');
+  }
+});
