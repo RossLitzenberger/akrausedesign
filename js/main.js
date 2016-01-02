99a52f592f38851bd0bc85f8a6764b2ec00a464f
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-02 14:40:01 -0600

    add testing cases

--- js/main.js
+++ js/main.js
@@ -8,90 +8,11 @@ var class_selection = [
 ];
 
 var i = 0;
-/*
-$('.home_btn').click(function(){
-  if($('.about_me').is(':visible') && $('.about_me').hasClass('active')){
-    $('.about_me').slideToggle('active');
-    $('.about_me').toggleClass('active');
-
-  }else{
-    $(this).toggleClass('active');
-  }
-});
-
-$('#discover').click(function(){
-
-  if(!$('.about_btn').hasClass('active')){
-    $('.about_btn').addClass('active');
-  }
-  else{
-    $('.about_btn').removeClass('active');
-  }
-
-  $('.about_me').slideToggle("fast");
-  $('.home_btn').toggleClass('active');
-  $('.about_btn').toggleClass('active');
-});
 
 
-$('.about_btn').click(function(){
-  $('.about_me').slideToggle('fast');
-  $('.home_btn').toggleClass('active');
-  $(this).toggleClass('active');
-});
 
-$('.about_me').click(function(){
-  // hide any pages in front of this if it is clicked
-  if($('#blogs').is(':visible')){
-    $('#blogs').slideToggle('fast');
-    console.log('blog page was hidden');
-
-    if($('#contact').is(':visible')){
-      $('#contact').slideToggle('fast');
-      console.log('contact page was hidden');
-    }
-    else{
-      console.log('contact page is not opened');
-    }
-
-    console.log('pages hidden');
-  }
-  else if($('#contact').is(':visible')){
-    $('#contact').slideToggle('fast');
-    console.log('contact page was hidden')
-  }
-  else{
-    $(this).slideToggle();
-    $('.about_me').removeClass('active');
-    $('.home_btn').toggleClass('active');
-    console.log('active buttons changed');
-  }
-});
-
-$('.blog_btn').click(function(){
-  $('#blogs').slideToggle('fast');
-});
-
-$('#blogs').click(function(){
-  if($(this).is(":visible")){
-    $(this).slideToggle('fast');
-    console.log('blog page was hidden');
-  }
-});
-$('.contact_btn').click(function(){
-  $('#contact').slideToggle('fast');
-  console.log('contact page was hidden');
-});
-
-$('#contact').click(function(){
-  if($(this).is(':visible')){
-    $(this).slideToggle('fast');
-  }
-});
-
-*/
 // when button is clicked, add active class to it
-var class_add = function(){
+var class_add = function(btn){
   $(this).addClass('active');
   // check to see if the class were added or not
   if(!($(this).hasClass(""))){
@@ -114,31 +35,33 @@ var class_remove = function(class_name){
 function active_add(click_option){
   switch(click_option){
     case 1:
-      class_add();
+      $('.home_btn').addClass('active');
       console.log('option 1 clicked');
+
 
 
       break;
     case 2:
-      class_add();
+      $('.about_btn').addClass('active');
       console.log('option 2 clicked');
 
 
       break;
     case 3:
-      class_add();
+      $('.blog_btn').addClass('active');
       console.log('option 3 clicked');
 
 
       break;
     case 4:
-      class_add();
+      $('.create_btn').addClass('active');
+      console.log('page not created yet, will link to admin.html');
       console.log('option 4 clicked');
 
 
       break;
     case 5:
-      class_add();
+      $('.contact_btn').addClass('active');
       console.log('option 5 clicked');
 
 
@@ -151,6 +74,7 @@ function active_add(click_option){
   };
 };
 
+/*
 
 if($('.home_btn').is('clicked')){
   for(i in class_selection){
@@ -195,3 +119,4 @@ else if($('.contact_btn').is('clicked')){
 else{
   console.log('error, no classes were added at all. please reload.');
 }
+*/
