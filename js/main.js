62b8c3822d9724b6d3469873ccdffee1b575be49
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-02 13:53:21 -0600

    add console tests

--- js/main.js
+++ js/main.js
@@ -36,23 +36,27 @@ $('.about_me').click(function(){
   // hide any pages in front of this if it is clicked
   if($('#blogs').is(':visible')){
     $('#blogs').slideToggle('fast');
+    console.log('blog page was hidden');
 
     if($('#contact').is(':visible')){
       $('#contact').slideToggle('fast');
+      console.log('contact page was hidden');
     }
     else{
-      console.log('contact is already hidden');
+      console.log('contact page is not opened');
     }
 
     console.log('pages hidden');
   }
   else if($('#contact').is(':visible')){
     $('#contact').slideToggle('fast');
+    console.log('contact page was hidden')
   }
   else{
     $(this).slideToggle();
     $('.about_me').removeClass('active');
     $('.home_btn').toggleClass('active');
+    console.log('active buttons changed');
   }
 });
 
@@ -63,10 +67,12 @@ $('.blog_btn').click(function(){
 $('#blogs').click(function(){
   if($(this).is(":visible")){
     $(this).slideToggle('fast');
+    console.log('blog page was hidden');
   }
 });
 $('.contact_btn').click(function(){
   $('#contact').slideToggle('fast');
+  console.log('contact page was hidden');
 });
 
 $('#contact').click(function(){
