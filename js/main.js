f1e972247a15a427b81c4a24d4d906b402c89bb1
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-02 14:30:58 -0600

    compress if/else statements into switch statement

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 
+var class_selection = [
+  $('.home_btn'), // 0
+  $('.about_btn'), // 1
+  $('.blog_btn'), // 2
+  $('.create_btn'), // 3
+  $('.contact_btn') // 4
+];
 
-
+var i = 0;
+/*
 $('.home_btn').click(function(){
   if($('.about_me').is(':visible') && $('.about_me').hasClass('active')){
     $('.about_me').slideToggle('active');
@@ -81,14 +89,24 @@ $('#contact').click(function(){
   }
 });
 
+*/
+// when button is clicked, add active class to it
 var class_add = function(){
   $(this).addClass('active');
+  // check to see if the class were added or not
+  if(!($(this).hasClass(""))){
+    console.log('class added')
+  }
+  else{
+    console.log('no classes added');
+  }
 }
 
 
-
-var class_remove = function(){
+// when a button is clicked, remove all instances of active besides the one that was clicked
+var class_remove = function(class_name){
   $(this).removeClass('active');
+  console.log('class removed');
 };
 
 
@@ -97,21 +115,83 @@ function active_add(click_option){
   switch(click_option){
     case 1:
       class_add();
+      console.log('option 1 clicked');
+
+
       break;
     case 2:
       class_add();
+      console.log('option 2 clicked');
+
+
       break;
     case 3:
       class_add();
+      console.log('option 3 clicked');
+
+
       break;
     case 4:
       class_add();
+      console.log('option 4 clicked');
+
+
       break;
     case 5:
       class_add();
+      console.log('option 5 clicked');
+
+
       break;
     default:
       $('home_btn').addClass('active');
+
+
       break;
   };
 };
+
+
+if($('.home_btn').is('clicked')){
+  for(i in class_selection){
+    if(i == 0){
+      i++;
+    }
+    class_remove(i);
+  }
+}
+else if($('.about_btn').is('clicked')){
+  for(i in class_selection){
+    if(i == 1){
+      i++;
+    }
+    class_remove(i);
+  }
+}
+else if($('.blog_btn').is('clicked')){
+  for(i in class_selection){
+    if(i == 2){
+      i++;
+    }
+    class_remove(i);
+  }
+}
+else if($('.create_btn').is('clicked')){
+  for(i in class_selection){
+    if(i == 3){
+      i++;
+    }
+    class_remove(i);
+  }
+}
+else if($('.contact_btn').is('clicked')){
+  for(i in class_selection){
+    if(i == 4){
+      i++;
+    }
+    class_remove(i);
+  }
+}
+else{
+  console.log('error, no classes were added at all. please reload.');
+}
