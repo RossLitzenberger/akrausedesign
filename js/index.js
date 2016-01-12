eb2b8bc0f921a42723cedd5e0afeab3af9fa616a
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-12 14:25:40 -0600

    define script work

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var blogs = [$('.top'), $('.middle'), $('.bottom')];
+
+// when a blog is hovered, remove blur effect
+// and add it to others.
+
+// after hover, add blurs back to the original states
