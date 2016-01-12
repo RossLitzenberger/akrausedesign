b2037f8f57052a56658b0222bc911a081ee840da
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-12 14:47:47 -0600

    add basic if else structure for hover effect

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
-var blogs = [$('.top'), $('.middle'), $('.bottom')];
+$(document).ready(function(){
+  var i = 0;
 
-// when a blog is hovered, remove blur effect
-// and add it to others.
+  var top = $('.top');
+  var middle = $('.middle');
+  var bottom = $('.bottom');
 
-// after hover, add blurs back to the original states
+  // when a blog is hovered, remove blur effect
+  // and add it to others.
+
+  // after hover, add blurs back to the original states
+
+  // if else statements for beginning of hover
+  if(top.is(':active')){
+
+    if(!(top.hasClass('blur'))){
+      top.removeClass('blur');
+      console.log('blur removed');
+    }
+    else if(!(middle.hasClass('blur'))){
+      middle.addClass('blur');
+      console.log('blur added');
+    }
+    else if(!(bottom.hasClass('blur'))){
+      bottom.addClass('blur');
+      console.log('blur added');
+    }
+
+  }
+
+  else if(middle.is(':active')){
+    top.addClass('blur');
+    middle.removeClass('blur');
+    bottom.addClass('blur');
+
+  }
+  else if(bottom.is(':active')){
+    top.addClass('blur');
+    middle.addClass('blur');
+    bottom.removeClass('blur');
+  }
+});
