0b74fb133afbc715fad9466788e382365a14a04f
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-13 12:03:30 -0600

    add hover functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-$(window).onLoad(function(){
+$(window).load(function(){
   $('.top').removeClass('blur');
   $('.middle').addClass('blur');
   $('.bottom').addClass('blur');
@@ -12,23 +12,47 @@ $(document).ready(function(){
   var middle = $('.middle');
   var bottom = $('.bottom');
 
+  function importance(){
+    $(this).css('z-index','3');
+  }
+
   // when a blog is hovered, remove blur effect
   // and add it to others.
 
   // after hover, add blurs back to the original states
 
   // if else statements for beginning of hover
+  top.hover(function(){
+    $(this).removeClass('blur');
+    middle.addClass('blur');
+    bottom.addClass('blur');
+    importance();
+  }, function(){
+    $(this).removeClass('blur');
+    middle.addClass('blur');
+    bottom.addClass('blur');
+  });
 
-
-  else if(middle.is(':active')){
+  middle.hover(function(){
     top.addClass('blur');
     middle.removeClass('blur');
     bottom.addClass('blur');
+    importance();
+  }, function(){
+    top.removeClass('blur');
+    middle.addClass('blur');
+    bottom.addClass('blur');
+  });
 
-  }
-  else if(bottom.is(':active')){
+  bottom.hover(function(){
     top.addClass('blur');
     middle.addClass('blur');
     bottom.removeClass('blur');
-  }
+    importance();
+  }, function(){
+    top.removeClass('blur');
+    middle.addClass('blur');
+    bottom.addClass('blur');
+  });
+
 });
