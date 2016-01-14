01344481664ae28d33fb84975b60fd325f47dfba
Andrew Krause <andrew.l.krause@gmail.com>
2016-01-14 00:27:13 -0600

    add blurReset function

--- js/index.js
+++ js/index.js
@@ -12,15 +12,39 @@ $(document).ready(function(){
   var middle = $('.middle');
   var bottom = $('.bottom');
 
+  function addBlur(t,m,b){
+    t.removeClass('blur');
+    m.addClass('blur');
+    b.addClass('blur');
+  }
+
+  function blurReset(t,m,b){
+    t.removeClass('blur');
+    m.addClass('blur');
+    b.addClass('blur');
+  }
+
   function reorder(important, mid, btm){
+
+    // change z-index importance
     important.css('z-index','3');
     mid.css('z-index','2');
     btm.css('z-index','1');
+
+    // add blurs
+    important.removeClass('blur');
+    mid.addClass('blur');
+    btm.addClass('blur');
   }
   function unorder(){
+
+    // change z-index importance back to normal
     top.css('z-index','3');
     middle.css('z-index','2');
     bottom.css('z-index','1');
+
+    // put blurs back to normal
+    blurReset(top, middle, bottom);
   }
 
   // when a blog is hovered, remove blur effect
