c1046d45ceef438d0f58a80a02be4bdc143189a2
Andrew Krause <andrew.l.krause@gmail.com>
2016-03-17 03:54:14 -0500

    finish all responsiveness

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  $('.mobile-btn').click(function(){
+
+    $('.main_nav').slideToggle();
+
+    $('.main_nav a').click(function(){
+
+      $('.main_nav').slideToggle();
+
+    });
+    if($('.main_nav a:first-child').is('click')){
+      $('.main_nav').slideToggle();
+    }
+  });
+});
