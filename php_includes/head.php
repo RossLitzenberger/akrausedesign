ef41e3ee6a5480b01b84829caa83249f4520c2b9
Andrew Krause <andrew.l.krause@gmail.com>
2016-04-06 01:57:20 -0500

    move files to a php format

---
+++ php_includes/head.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link type='text/css' rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700'>
+<link type='text/css' rel='stylesheet' href='css/stylesheet.css'>
+<meta charset="utf-8">
+<meta name='author' content='Andrew Krause'>
+<meta name='description' content='Will Andrew really remember to change this description? Or will he just let you wonder what is really going on in his head? Who knows...'>
+<meta name='keywords' content=''>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<meta name="google-site-verification" content="H6BcLV_rJDx1RltyT2LyVevCcjCesgyxWBRby4dlaf8" />
